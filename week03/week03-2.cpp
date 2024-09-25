242616cc3ecce0d346c33338d81d87350d971920
sweetyhank <145088436+sweetyhank@users.noreply.github.com>
2024-09-25 11:43:07 +0800

    Add files via upload

---
+++ week03/week03-2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace 
+()
+{
+	printf("%d\n",'a');
+	printf("%d\n.""a");
+	if("a"==97) printf("其實'a'是質數 97\n");
+}
---
+++ week03/week03-4.cpp
@@ -0,0 +1,7 @@
+//week03-4.cpp
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        return haystack.find(needle);
+    }
+};
