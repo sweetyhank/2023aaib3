6f74fed6903cc2033e66292e7b90a0f51500b5ad
sweetyhank <145088436+sweetyhank@users.noreply.github.com>
2024-10-16 11:56:18 +0800

    Add files via upload

---
+++ 新增資料夾/week06-2.a.cpp
@@ -0,0 +1,14 @@
+
+///week06-2.cpp
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> a;
+    int now;
+    while( cin >> now ){
+        a.push_back(now);
+        cout << now <<"\n";
+    }
+}
