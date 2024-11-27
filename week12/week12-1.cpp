e1a8c46e8e475d4b9ea6677a263034a44ab944fb
sweetyhank <145088436+sweetyhank@users.noreply.github.com>
2024-11-27 12:07:54 +0800

    Add files via upload

---
+++ week12/week12-1.cpp
@@ -0,0 +1,20 @@
+///week12-1.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+///struct Node();
+///class Cat{};
+struct Node{
+    int val;
+};
+class Cat{
+    public:
+        string name;
+};
+int main()
+{
+    Cat cat1;
+    cat1.name="cattie";
+    Node node;
+    node.val=1;
+}
